c65bda929d2448c836fd7baae2b96fca0e26f9c7
mayank sharma <mayank0611sharma@gmail.com>
2024-02-15 19:29:44 +0530

    commiting

--- class/java.js
+++ class/java.js
@@ -1,6 +1,25 @@
-// document.write("welcome to the world of javascript" + "<br>");
-// document.write("My roll number is ",10 + "<br>");
-// document.write("addition of 10+10 = ",10+10);
-function showmsg(){
-    document.getElementById("message").innerHTML="hello";
+
+// let a;
+// let x = 3;
+// let y = 3;
+// let z = x+y;
+// console.log(z);
+// let x = 31;
+// let y = 15;
+// x = x+y;
+// y = x-y;
+// x = x-y;
+// console.log(x);
+// console.log(y);
+
+let x = 5;
+function a(){
+    let x = 100;
+    b();
 }
+function b(){
+    let y = 50;
+    let sum = x+y;
+    console.log("sum is : ",sum);
+}
+a();
