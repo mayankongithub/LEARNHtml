7c05e8ee29a7719288b385ba9b8a6ce52d7c617d
mayank sharma <mayank0611sharma@gmail.com>
2024-02-20 19:39:00 +0530

    commiting

--- class/java.js
+++ class/java.js
@@ -24,8 +24,49 @@
 // }
 // a();
 
-var num1 = parseInt(prompt("enter 1st number: "));
-var num2 = parseInt(prompt("enter 2nd number: "));
-let sum = num1 + num2;
-console.log("sum is: ",sum);
-console.log(typeof sum);
+// var num1 = parseInt(prompt("enter 1st number: "));
+// var num2 = parseInt(prompt("enter 2nd number: "));
+// let sum = num1 + num2;
+// console.log("sum is: ",sum);
+// console.log(typeof sum);
+
+// var i = 20;
+// if(i>15){
+//     console.log("greater than 15");
+// }
+// console.log("not in if");
+
+// var i = 10;
+// if(i>15){
+//     console.log("greater than 15");
+// }
+// else console.log("less than 15");
+
+// var i = 10;
+// if(i==10){
+//     console.log("=to 10");
+//     if(i<15){
+//         console.log("<15");
+//     }
+// }
+// else{
+//     console.log("not = 10");
+// }
+
+// var i = 10;
+// if(i<10){
+//     console.log("less than 10");
+// }
+// else if(i>10){
+//     console.log("greater than 10");
+// }
+// else{
+//     console.log("equal to 10");
+// }
+var i = praceInt(prompt('enter a number'));
+if(i%2==0){
+    document.write("<h1> is an Even Number</h1>");
+}
+else{
+    document.write(" <h1>is an odd Number</h1>");
+}
